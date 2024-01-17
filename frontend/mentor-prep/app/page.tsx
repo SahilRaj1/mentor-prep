dbf07a85f90d0f05b2d937aaa8c17c95765911ef
AnishZedex <anish.deshpande@zedexinfo.com>
2024-01-17 14:04:22 +0530

    nav

---
+++ frontend/mentor-prep/app/page.tsx
@@ -0,0 +1,8 @@
+import { HeaderMegaMenu } from "../components/HeaderMegaMenu/HeaderMegaMenu";
+
+export default function HomePage() {
+  return <>
+  <HeaderMegaMenu />
+  <div>Home page</div>
+  </>;
+}
