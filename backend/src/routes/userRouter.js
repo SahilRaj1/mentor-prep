3e9dbb9807abe7fa9592e9ae03f2426e8dbc613a
SahilRaj1 <raj24sahil@gmail.com>
2024-01-19 02:26:13 +0530

    image upload fix

--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -6,6 +6,6 @@ import uploadImage from "../middlewares/uploadImage.js";
 const router = Router();
 
 router.get('/', validateToken, getAllUsers);
-router.put('/:id', validateToken, uploadImage.single("img"), updateUserController);
+router.put('/:id', validateToken, uploadImage.single("avatar"), updateUserController);
 
 export default router;
